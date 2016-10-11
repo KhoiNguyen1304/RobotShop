c8d93c7b95e5a18157fb501f578af25aeab13b6b
KhoiNguyen1304 <mistycat1304@gmail.com>
2016-10-10 20:22:21 -0500

    Re-uploaded Torse and Head header files

---
+++ src_code/Head.h
@@ -0,0 +1,11 @@
+#ifndef __HEAD_H
+#define __HEAD_H 2016
+
+#include "RobotPart.h"
+#include "PartType.h"
+
+class Head: public RobotPart {
+	public:
+		Head(string name, int partNumber, double weight, double cost,
+			 string description, PartType partType);
+};
